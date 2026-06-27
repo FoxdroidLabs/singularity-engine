270bd77e94a3e42bd3aa9d2d6dd182face66531b
Foxdroid - Vaelen <foxdroidlabs@gmail.com>
2026-06-27 14:52:35 +0200

    comment modified

--- src/launcher/core/settings.zig
+++ src/launcher/core/settings.zig
@@ -3,6 +3,6 @@
 // Write a settings.cfg
 // Windows projects : Stored in Documents/Projects by default
 // Linux projects : Stored in Documents/Projects by default
-// Possibility to change the projects folder and save it 
+// Possibility to change the projects folder and save it
 // Settings will also have other things like the shader rebuild for the engine/projects if anything is corrupted
-// Maybe a project repair too
+// Maybe a project repair too (need to see when launcher functions are done)
