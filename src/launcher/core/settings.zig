8e2dacd85cf965f26d3b99cb48bccd5693b73bbd
Foxdroid - Vaelen <foxdroidlabs@gmail.com>
2026-06-30 10:35:00 +0200

    Clean some code

--- src/launcher/core/settings.zig
+++ src/launcher/core/settings.zig
@@ -10,22 +10,18 @@ const mem = std.mem;
 const builtin = @import("builtin");
 
 pub const Settings = struct {
-    const buf_size = 1024 * 4;
-    var buffer: [buf_size]u8 = undefined;
+    var buffer: [4096]u8 = undefined;
 
-    // Settings File
     pub fn settingsInit(io: std.Io, allocator: std.mem.Allocator, environ: *std.process.Environ.Map) !void {
-        const home = environ.get("HOME") orelse environ.get("USERPROFILE") orelse return error.HomeNotFound;
-
         var createConfigFile = std.Io.Dir.createFile(std.Io.Dir.cwd(), io, "settings.cfg", .{ .read = true, .exclusive = true }) catch |errfile|
             switch (errfile) {
                 error.PathAlreadyExists => {
-                    std.log.warn("The Settings File already exist", .{});
                     return;
                 },
                 else => return errfile,
             };
 
+        const home = environ.get("HOME") orelse environ.get("USERPROFILE") orelse return error.HomeNotFound;
         const docs_path = try std.fs.path.join(allocator, &[_][]const u8{ home, "Documents", "Singularity Projects" });
         defer allocator.free(docs_path);
         const projects_path = try std.fmt.allocPrint(allocator, "projects_path: {s}", .{docs_path});
@@ -37,14 +33,12 @@ pub const Settings = struct {
         try FirstWriteConfigFile.seekTo(0);
         defer createConfigFile.close(io);
 
-        // Project Dir
         const home_dir = try std.Io.Dir.cwd().openDir(io, home, .{});
         defer home_dir.close(io);
         const documents_dir = try home_dir.openDir(io, "Documents", .{});
         defer documents_dir.close(io);
         documents_dir.createDir(io, "Singularity Projects", .default_dir) catch |errfolder| switch (errfolder) {
             error.PathAlreadyExists => {
-                std.log.warn("The Projects Folder has already been created", .{});
                 return;
             },
             else => return errfolder,
